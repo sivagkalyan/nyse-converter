b359a5a78f901dda037042a3f79107cf5ca7eb5a
sivagkalyan <kalyansiva118.com>
2023-10-30 22:21:26 +0530

    Core logic to convert CSV to JSON

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-def hw():
-    print('Hello World')
-    return
+from dask import dataframe as dd
 
 
-if __name__== '__main__':
-    hw()
+def main():
+    print('File format conversion started')
+    df = dd.read_csv(
+        'data/nyse_all/nyse_data/NYSE*.txt.gz',
+        names=['ticker', 'trade_date', 'open_price', 'low_price',
+               'high_price', 'close_price', 'volume'],
+        blocksize=None
+    )
+    print('Dataframe is created and will be writen in json format')
+    df.to_json(
+        'data/nyse_all/nyse_json/part-*.json.gz',
+        orient='records',
+        lines=True,
+        compression='gzip',
+        name_function=lambda i: '%05d' % i
+    )
+    print('File format conversion completed')
+
+
+if __name__ == '__main__':
+    main()
