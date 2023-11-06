ddd916d1c62ac9532be8c8ce43afd5a31381e4c7
sivagkalyan <kalyansiva118.com>
2023-11-06 12:33:35 +0530

    Added logging

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import os
 import glob
+import logging
 from dask import dataframe as dd
 
 
 def main():
-    print('File format conversion started')
+    logging.basicConfig(
+        filename='logs/ffc.log',
+        level=logging.INFO,
+        format='%(levelname)s %(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %I:%M:%S %p'
+    )
+    logging.info('File format conversion started')
     src_dir = os.environ['SRC_DIR']
     #tgt_dir = os.environ['TGT_DIR']
     src_file_names = sorted(glob.glob(f'{src_dir}/NYSE*.txt.gz'))
@@ -18,14 +25,14 @@ def main():
                'high_price', 'close_price', 'volume'],
         blocksize=None
     )
-    print('Dataframe is created and will be writen in json format')
+    logging.info('Dataframe is created and will be writen in json format')
     df.to_json(
         tgt_file_names,
         orient='records',
         lines=True,
         compression='gzip'
     )
-    print('File format conversion completed')
+    logging.info('File format conversion completed')
 
 
 if __name__ == '__main__':
